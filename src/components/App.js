a4fc00b0403790775ecbcc98633070e8f9e97794
Ömer Faruk Cavdar <omer@sorunapp.com>
2020-06-05 00:03:12 +0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import UserCreate from './UserCreate';
+import LanguageContext from '../context/LanguageContext';
+import ColorContext from '../context/ColorContext';
+import LanguageSelector from './LanguageSelector';
+
+class App extends React.Component {
+    state = { language: 'english' }
+
+    onLanguageChange = (language) => {
+        this.setState({ language })
+    }
+
+    render() {
+        return (
+            <div className="ui container">
+                <LanguageSelector onLanguageChange={this.onLanguageChange} />
+                <ColorContext.Provider value="purple">
+                    <LanguageContext.Provider value={this.state.language}>
+                        <UserCreate />
+                    </LanguageContext.Provider>
+                </ColorContext.Provider>
+            </div>
+        );
+    }
+}
+
+export default App;
